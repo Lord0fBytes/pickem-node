75e3ddd25cc2a3329271dbf83c302c0ded39ea17
Justin Masse <jmasse@webtron.me>
2017-04-29 00:27:45 -0400

    finally fixed

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
 
 var ctlrAPI = require('../controller/apiController.js');
 
 router.get('/picks/:weeknum', ctlrAPI.getAllPicks);
 
 router.get('/picks/:user/:weeknum', ctlrAPI.getMyPicks);
-=======
-//var cookieParser = require('cookie-parser');
-
-//router.use(cookieParser());
-
-router.use('/api', function(req, res){
-  res.json({
-    name:req.cookies.name,
-    url: "something"
-  });
-})
->>>>>>> 7bb1ff9426574278904b805ef3784c22f2efd312
 
 module.exports = router;
